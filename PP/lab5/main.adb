445650a4d746e103f5d0f9e37358de2d0c91e760
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-04-19 14:18:12 +0300

    Added 3 part of PP couse project (programm) + lab5 PP

--- PP/lab5/main.adb
+++ PP/lab5/main.adb
@@ -5,16 +5,15 @@ use Ada.Text_IO, Ada.Integer_Text_IO,Ada.Float_Text_IO, Ada.Synchronous_Task_Con
 ------------------------------------------------------------------
 --                                                              --
 --          Програмування паралельних компьютерних систем       --
---           Курсова робота. ПРГ1. Ада. Захищені модулі         --
+--          Лаб 5. Ада. Захищені модулі					        --
 --                                                              --
---  Файл: parallelCalcs.ada                                     --
+--  Файл: main.abd			                                    --
 --  Завдання:                                                   --
---        MA = MB*(MC+MO)*a + min(Z)*MK		                   	--
 --                               								--
 --        								                    	--
 --                                                              --
 --  Автор: Кахерський Олег, група IП-31                         --
---  Дата: 12.03.2016                                            --
+--  Дата: 07.04.2016                                            --
 --                                                              --
 ------------------------------------------------------------------
 
@@ -22,7 +21,6 @@ procedure Main is
 	n: Integer; 			--розмірність матриць та векторів
 	p: Integer; 			--кількість задач
 	h: Integer; 			--кількість рядків матриць та векторів на одну задачу
-	scalarResults : array (1..P) of Integer;
 	procedure Start is
 		package MatrixOperationsN is new MatrixOperations(N);
 		use MatrixOperationsN;
@@ -33,7 +31,6 @@ procedure Main is
 		protected ProtectedModule is
 			entry waitInput; 					--бар'єр для синхронізації вводу даних
 			entry waitScalarMultFin;			--очікування підсумовування скалярних добутків підвекторів
-			entry waitScalarPreparationFin;     --очікування обчислення скалярних добутків підвекторів
 			entry readyForOuptut; 				--очікування сигналу від T2-Tp задачею Т1
 
 			function readE return Integer;
@@ -44,9 +41,8 @@ procedure Main is
 
 			procedure inputFinishSignal;		--надсилання сигналу про завершення вводу даних
 			procedure finishCalcsSignal;		--надсилання сигналу про завершення обчислень
-			procedure finishScalarPreparation;        --надсилання сигналу про завершення підсумовування проміжних результатів скалярного добутку
 			procedure finishScalarMultCalcs;    --надсилання сигналу про завершення обчислення скалярного добутку векторів
-			procedure init_a(b: Integer);
+			procedure addToScalarMultResult(b: Integer);
 			procedure init_e(b: Integer);
 			procedure init_d(b: Integer);
 			procedure initR(Buff: Vector);
@@ -55,33 +51,27 @@ procedure Main is
 		private
 			inputFlag : Integer := 0;
 			finishFlag : Integer := 0;
-			scalarPreparation : Integer := 0;
 			scalarResultFlag : Integer := 0;
 
-			e, scalarMultResult, d : Integer;
+			e, x, d : Integer;
 			R : Vector;
 			MK : Matrix;
 		end ProtectedModule;
 
 		protected body ProtectedModule is
 
-			--розблокування задач при inputFlag = 2 (завершення вводу даних в T1..4)
+			--розблокування задач при inputFlag = 4 (завершення вводу даних в T1..4)
 			entry waitInput when inputFlag = 4 is
 			begin
 				null;
 			end waitInput;
 
-			entry waitScalarPreparationFin when scalarPreparation = p is
-			begin
-				null;
-			end waitScalarPreparationFin;
-
-			entry waitScalarMultFin when scalarResultFlag = 1 is
+			entry waitScalarMultFin when scalarResultFlag = p is
 			begin
 				null;
 			end waitScalarMultFin;
 
-			--розблокування Т1 при завершенні обчислень в задачах T2..Tp
+			--розблокування Т2 при завершенні обчислень в задачах T1..Tp
 			entry readyForOuptut when finishFlag = p-1 is
 			begin
 				null;
@@ -99,7 +89,7 @@ procedure Main is
 
 			function read_A return Integer is
 			begin
-				return scalarMultResult;
+				return x;
 			end read_A;
 
 			function readR return Vector is
@@ -122,20 +112,15 @@ procedure Main is
 				finishFlag := finishFlag + 1;
 			end finishCalcsSignal;
 
-			procedure finishScalarPreparation is        --надсилання сигналу про завершення підсумовування проміжних результатів скалярного добутку
-			begin
-				scalarPreparation := scalarPreparation + 1;
-			end finishScalarPreparation;
-
 			procedure finishScalarMultCalcs is
 			begin
-				scalarResultFlag := 1;
+				scalarResultFlag := scalarResultFlag + 1;
 			end finishScalarMultCalcs;
 
-			procedure init_a(b: Integer) is
+			procedure addToScalarMultResult(b: Integer) is
 			begin
-				scalarMultResult := b;
-			end init_a;
+				x := x + b;
+			end addToScalarMultResult;
 
 			procedure init_e(b: Integer) is
 			begin
@@ -164,14 +149,6 @@ procedure Main is
 			end outputLine;
 		end ProtectedModule;
 
-		function scalarResultsSum return Integer is
-			Result : Integer := 0;
-		begin
-			for I in 1..P loop
-				Result := Result + scalarResults(I);
-			end loop;
-			return Result;
-		end scalarResultsSum;
 		procedure calcMatrixEquation(ai,ei,di,startIndex,endIndex : Integer; Ri:Vector; MKi: Matrix) is
 			Buf1: Matrix;
 			Vect_Buf: Vector;
@@ -183,7 +160,7 @@ procedure Main is
 
 		task body ThreadTask is
 			i: Integer := tID;
-			scalarMultResult_i, ei, di, startIndex, endIndex: Integer;
+			xi, ei, di, startIndex, endIndex: Integer;
 			MKi : Matrix;
 			Ri: Vector;
 		begin
@@ -245,33 +222,28 @@ procedure Main is
 			else
 				ProtectedModule.outputLine("Task "& Integer'Image(i) & " waits for inputting data in T1, Tp");
 			end if;
+
+			--синхронізація по вводу
 			ProtectedModule.waitInput; 
-			scalarResults(i) := Scalar_Multiply(B, C, startIndex, endIndex);
-			ProtectedModule.finishScalarPreparation;
-			if i = 1 then
-				For j in 1..p loop
-					Put(Integer'Image(ScalarResults(J)));
-					New_Line;
-				end loop;
-				ProtectedModule.waitScalarPreparationFin;
-				ProtectedModule.init_a(scalarResultsSum);
-				ProtectedModule.outputLine("scalar multiplication result = " & Integer'Image(ProtectedModule.read_a));
-				ProtectedModule.finishScalarMultCalcs;
-			end if;
+
+			--обчислення скалярного добутку
+			ProtectedModule.addToScalarMultResult(Scalar_Multiply(B, C, startIndex, endIndex));
+
+			--синхронізація по завершенню обчислення скалярного добутку
+			ProtectedModule.finishScalarMultCalcs;
 			ProtectedModule.waitScalarMultFin;
-			ProtectedModule.outputLine("Task "& Integer'Image(i) & " is copying shared resources");
 
 			--копіювання СР
+			ProtectedModule.outputLine("Task "& Integer'Image(i) & " is copying shared resources");
 			ei := ProtectedModule.readE;
 			di := ProtectedModule.readD;
 			Ri := ProtectedModule.readR;
-			scalarMultResult_i := ProtectedModule.read_a;
+			xi := ProtectedModule.read_a;
 			MKi := ProtectedModule.readMK;
 
 			ProtectedModule.outputLine("Task "& Integer'Image(i) & " is calculating matrix equation");
-
 			
-			calcMatrixEquation(scalarMultResult_i, ei, di, startIndex, endIndex, Ri, MKi);
+			calcMatrixEquation(xi, ei, di, startIndex, endIndex, Ri, MKi);
 
 			if i = 2 then
 				ProtectedModule.outputLine("Task "& Integer'Image(i) & " is waiting for signal from all(finishing calc matrix equation)");
@@ -295,7 +267,6 @@ procedure Main is
 			tArray: TaskArray;
 		begin	
 			for I in 1 .. p loop
-				scalarResults(I) := 0;
 				tArray(I) := new ThreadTask(I);
 			end loop;
 		end Start;
